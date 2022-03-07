d91ade8712d143b9c54e535658538795258dca57
Nayhee <nathantraczewski@gmail.com>
2022-03-07 11:05:31 -0600

    added modulo functions

--- scripts/fish/FishData.js
+++ scripts/fish/FishData.js
@@ -33,3 +33,43 @@ const fishCollection = [
 export const getFish = () => {
     return fishCollection;
 }
+
+
+export const getMostHolyFish = (fishArray) => {          //needs to have the fishCollection array passed in when calling this function!
+    // 3, 6, 9, 12, etc... fish
+    const holyFishArray = []
+
+    for (const fish of fishArray) {
+        if (fish.length % 3 === 0) {
+            holyFishArray.push(fish);
+        }
+    }
+
+    return holyFishArray;
+}
+
+export const getSoldierFish = (fishArray) => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiersArray = []
+
+    for (const fish of fishArray) {
+        if(fish.length % 5 === 0) {
+            soldiersArray.push(fish);
+        }
+    }
+
+
+    return soldiersArray
+}
+
+export const getUnworthy = (fishArray) => {
+    // Any fish not a multiple of 3 or 5
+    const unworthyArray = []
+
+    for (const fish of fishArray) {
+        if (fish.length % 5 != 0 || fish.length % 3 != 0) {
+            unworthyArray.push(fish);
+        }
+    }
+    return unworthyArray;
+}
